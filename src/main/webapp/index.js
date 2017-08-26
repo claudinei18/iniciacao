579fcbf5d9defd508d1981c3ca0bd7d9cf7b97c0
claudinei18 <claudineigom18@gmail.com>
2017-08-26 18:16:31 -0300

    Update

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -26,7 +26,7 @@ angular.module('contagem-regressiva', ['timer'])
             {id: "17", nome: "Prova 2 - Filosofia", data: new Date( "12/05/2017")},
             {id: "18", nome: "Formulário Ponto de Controle preenchido", data: new Date( "08/21/2017")},
             {id: "18", nome: "Introdução, referencial Teórico e Metodologia.", data: new Date( "09/18/2017")},
-            {id: "19", nome: "Implementação Física Início dos testes.", data: new Date( "09/10/2017")},
+            {id: "19", nome: "Implementação Física Início dos testes.", data: new Date( "10/09/2017")},
             {id: "20", nome: "Trabalho Final ", data: new Date( "11/27/2017")},
             {id: "21", nome: "Trabalho Final Impresso", data: new Date( "12/04/2017")},
 
